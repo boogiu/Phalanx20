5a47664fc7c4cdfd757ed6df3297fcd5ce3679b3
boogiu <boogiww@gmail.com>
2024-03-01 17:23:59 +0900

    인트로 페이지 수정

--- src/Pages/TestPreMessage.jsx
+++ src/Pages/TestPreMessage.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { SectionBlock2, TextGruop, RedSpan,StrongTxt,StyledContainer, SectionBlock, BackGroundSrc, HannaTxt, GrenBlock } from '../components/common/GlobalStyle';
+import {TransText, ImgComponent,HannaProTxt,TextGruop3, SectionBlock3, TextGruop2, Balloon, GradientButton, SectionBlock2, TextGruop, RedSpan,StrongTxt,StyledContainer, SectionBlock, BackGroundSrc, HannaTxt, GrenBlock } from '../components/common/GlobalStyle';
 import styled,{css,keyframes} from 'styled-components';
 import { fadeInFromLeft } from "../components/common/Animation"
 import IntersectionObserverComponent from '../components/common/IntersectionObserverComponent'
 import AnimatedLogo from '../components/Layout/PreMessage/AnimatedLogo'
-const TestPreMessage = () => {
+import { useNavigate } from 'react-router-dom';
+import ReactGA4 from 'react-ga4';
 
+const TestPreMessage = () => {
+  const navigate = useNavigate();
   const FirstRef = useRef(null);
   const SecondRef = useRef(null);
   const ThirdRef = useRef(null);
 
   const [isInViewport, setIsInViewport] = useState(false);   
+  const handleLinkClick = () => {
+
+        console.log('다음 페이지');
+        ReactGA4.event({
+            category: 'Intro Navigation',
+            action: 'new Intro Skipped',
+            label: 'new Intro Page Skipped'
+        });
+        navigate('/DefaultPage');
+    } 
 
   useEffect(() => {
     const observerCallback = (entries) => {
@@ -58,77 +71,125 @@ const TestPreMessage = () => {
             <AnimatedLogo/>
         </SectionBlock>
       </BackGroundSrc>
+
       <BackGroundSrc imagePath={"PreBack2.png"} >
-        
-         <SectionBlock >
-          <TextGruop>
-            <HannaTxt>기획...? <RedSpan>그게 뭐야?</RedSpan></HannaTxt>
+         <SectionBlock2>
+            <Balloon>
+              <HannaTxt>기획...? <RedSpan>그게 뭐야?</RedSpan></HannaTxt>
+            </Balloon>
             <StrongTxt>하던 너</StrongTxt>
+            <Balloon>
             <HannaTxt>에이 바보들...이렇게 하면 <RedSpan>더 괜찮은데</RedSpan></HannaTxt>
+            </Balloon>
             <StrongTxt>하던 너!</StrongTxt>
+            <Balloon>
             <HannaTxt>이건 이렇게 바꾸는게<RedSpan> 더 좋은데?</RedSpan></HannaTxt>
+            </Balloon>
             <StrongTxt><Dinglediv3>
               하던 너!!
               </Dinglediv3></StrongTxt>
+              <Balloon>
             <HannaTxt>내가하면 <RedSpan>더 잘할 것 같은데?</RedSpan></HannaTxt>
+            </Balloon>
             <StrongTxt><Dinglediv4>
               하던 너!!!</Dinglediv4></StrongTxt>
-          </TextGruop>
-        </SectionBlock>
+        </SectionBlock2>
       </BackGroundSrc>
 
       <BackGroundSrc imagePath={"PreBack3.png"} >
-      <LeftSlideGroup ref={FirstRef} isInViewport={isInViewport}>
          <SectionBlock2>
-         <HannaTxt>여태것 너가 해온 <RedSpan>그런 기획?</RedSpan></HannaTxt>
-         <TextGruop>
-          <HannaTxt>우리는 그걸 <br/><RedSpan>"상상" 그리고 "취향"</RedSpan><br/>이라고 불러요</HannaTxt>
-         </TextGruop>
-            <HannaTxt>기획에는 방법과 단계가 있고</HannaTxt>
-            <HannaTxt>우리 팔랑크스는 그 방법과 단계를 <br/>찾아가는 사람들이에요!</HannaTxt>
-         
+          <ImgComponent imagePath={"back10.png"} 
+                  top={"12%"} 
+                  left={"65%"}
+                  rotate={"-30deg"}
+                  width={"50%"}/>
+          <HannaTxt><BiggerSpan>여태것 너가 해온 <RedSpan>그런 기획?</RedSpan></BiggerSpan></HannaTxt>
+          <TextGruop2>
+            <HannaProTxt>우리는 그걸 <br/><RedSpan><BiggerSpan>"상상" 그리고 "취향"</BiggerSpan></RedSpan><br/>이라고 불러요</HannaProTxt>
+          </TextGruop2>
+              <HannaTxt>기획에는 <BiggerSpan>
+                <GreenSpan>방법과 단계&nbsp;</GreenSpan></BiggerSpan>가 있고</HannaTxt>
+              <TextGruop>
+                <BingleDiv>
+              <HannaProTxt>
+                <GreenSpan>
+                  우리 팔랑크스는 그 방법과 단계를 <br/>
+                  찾아가는 사람들이에요!
+                </GreenSpan>
+              </HannaProTxt></BingleDiv>
+              </TextGruop>
+        </SectionBlock2>
+       
+        <SectionBlock3>
+        <ImgComponent imagePath={"MadGirl.png"} 
+          top={"53%"} 
+          left={"-20%"}
+          rotate={"0deg"}
+          width={"50%"}/>
+            <BiggerSpan>만약, 당신이 <RedSpan>기획의 천재</RedSpan>라서</BiggerSpan>
+            <BiggerSpan>그래서 기획의 방법따윈</BiggerSpan>
+         <Dinglediv4><TextGruop3>
+            <HannaTxt><BiggerSpan>개나 줘버리라면!!</BiggerSpan></HannaTxt>  
+          </TextGruop3></Dinglediv4>
+          <TransText><HannaProTxt>
+            <BiggestSpan>팔랑크스와는 아마 <br/><RedSpan>맞지 않을거에요😥</RedSpan></BiggestSpan>
+            </HannaProTxt>
+          </TransText>
           
-        </SectionBlock2>
-  
-        <SectionBlock2>
-            <HannaTxt>만약, 당신이 기획의 천재라서</HannaTxt>
-            <HannaTxt>그래서 기획의 방법따윈</HannaTxt>
-         <TextGruop>
-            <HannaTxt><RedSpan>개나 줘버리라면</RedSpan></HannaTxt>  
-          </TextGruop>
-            <HannaTxt>팔랑크스와는 아마 맞지 않을거에요</HannaTxt>
-        </SectionBlock2>
-        </LeftSlideGroup>
+        </SectionBlock3>
+        
       </BackGroundSrc>
 
       <BackGroundSrc imagePath={"PreBack4.png"} >
-         
-
+    
         <SectionBlock2>
-          <HannaTxt>하지만 당신에게</HannaTxt>
-        <TextGruop>
-            <HannaTxt>기획의 니즈를 파악하고</HannaTxt>
-            <HannaTxt>기획의 구조를 파악하고</HannaTxt>
-            <HannaTxt>기획의 내용을 심화하고</HannaTxt>
-            <HannaTxt>기획의 대상에게 제안하는</HannaTxt>      
-            
+        <ImgComponent imagePath={"Magic.png"} 
+                top={"0%"} 
+                left={"70%"}
+                rotate={"-24deg"}
+                width={"50%"}/>
+          <StrongTxt>
+            <BiggestSpan>하지만 당신에게<br/><br/></BiggestSpan></StrongTxt>
+        <TextGruop><HannaProTxt>
+            <MoreBigger>기획의 <GreenSpan>니즈를 파악하고</GreenSpan><br/></MoreBigger>
+            <MoreBigger>기획의 <GreenSpan>구조를 파악하고</GreenSpan><br/></MoreBigger>
+            <MoreBigger>기획의 <GreenSpan>내용을 심화하고</GreenSpan><br/></MoreBigger>
+            <MoreBigger>기획의 대상에게<br/> 
+            <GreenSpan><BiggestSpan>직접 제안하는</BiggestSpan></GreenSpan></MoreBigger>      
+            </HannaProTxt>
             </TextGruop>
-            <HannaTxt>기획 분야 최고의 대학생이 되고자하는</HannaTxt>      
-            <HannaTxt>그러고자 하는 의욕이 있다면</HannaTxt>
+            <ImgComponent imagePath={"back6.png"} 
+                top={"63%"} 
+                left={"73%"}
+                rotate={"-10deg"}
+                width={"30%"}/>
+          <TransText>
+            <HannaProTxt><MoreBigger><GreenSpan>기획 분야 최고의</GreenSpan><br/> 
+            대학생이 되고자하는<br/>
+            <GreenSpan>그러고자 하는 의욕이 있다면</GreenSpan></MoreBigger></HannaProTxt>
+            </TransText>
         </SectionBlock2>
+       
       </BackGroundSrc>
       
       <BackGroundSrc imagePath={"PreBack5.png"}>
          <SectionBlock2>
-            <HannaTxt>내가 배우고</HannaTxt>
-            <HannaTxt>직접 기획해보는</HannaTxt>
+         <ImgComponent imagePath={"heart5.png"} 
+                top={"-15%"} 
+                left={"-30%"}
+                rotate={"-14deg"}
+                width={"60%"}/>
+            <HannaProTxt>
+              <MoreBigger>내가 배우고<br/>
+              직접 기획해보는</MoreBigger></HannaProTxt>
             <TextGruop>
-            <HannaTxt>기획/컨설팅 분야 최고의 동아리</HannaTxt>
-            <HannaTxt>전국대학연합,</HannaTxt>
-            <HannaTxt>클럽 팔랑크스 </HannaTxt>
-            <HannaTxt>한번 구경해볼래요? </HannaTxt>
+            <HannaProTxt>기획/컨설팅 분야 최고의 동아리<br/>
+            <MoreBigger><GreenSpan>전국대학연합,<br/>
+            클럽 팔랑크스 </GreenSpan></MoreBigger></HannaProTxt>
           </TextGruop>
-        </SectionBlock2>
+          <HannaProTxt>한번 구경해볼래요? </HannaProTxt>
+          <GradientButton onClick={handleLinkClick}>팔랑크스 알아보기</GradientButton>
+        </SectionBlock2 >
       </BackGroundSrc>
       
     </StyledContainer>
@@ -175,11 +236,43 @@ const Dinglediv4 = styled.div`
   }
 `;
 
-
-const LeftSlideGroup = styled.section`
-    opacity : 0%;
-    animation: 
-  ${({ isInViewport }) => (isInViewport ? 
-    css`${fadeInFromLeft} 1s ease forwards` : 'none')};
+const BingleDiv = styled.div`
+  animation: BingleAnimation 2s infinite; /* 애니메이션을 적용합니다. */
+  width : 100%;
+  display: flex;
+  flex-flow : row wrap;
+  justify-content: center;
+  @keyframes BingleAnimation{
+    0% {
+      transform: rotate(0deg);
+    }
+    25% {
+      transform: rotate(-1deg)
+    }
+    75% {
+      transform: rotate(1deg)
+    }
+    100% {
+      transform: rotate(0deg)
+    }
   }
 `;
+
+
+const BiggerSpan = styled.span`
+  font-size : 7vmin;
+  font-style : italic;
+  font-family : "한나프로";
+`;
+const MoreBigger = styled.span`
+  font-size : 7vmin;
+  line-height : 200%;
+`;
+const BiggestSpan = styled.span`
+  font-size : 8vmin;
+  line-height : 280%;
+`;
+const GreenSpan = styled.span`
+color : #00FF00;
+
+`
--- src/components/common/GlobalStyle.jsx
+++ src/components/common/GlobalStyle.jsx
@@ -17,12 +17,15 @@ export const StyledContainer = styled.div`
 export const SectionBlock = styled.div`
   width: 100%;
   height: 100vh;
+  min-height: 650px;
   display: flex;
   flex-flow : row wrap;
   justify-content: center;
   align-items: center;
   color : white;
   position : relative;
+  margin: 10% 0% 0% 0%;
+  border : 1px solid red;
 `;
 export const SectionBlock2 = styled.div`
   width: 100%;
@@ -33,10 +36,27 @@ export const SectionBlock2 = styled.div`
   align-items: center;
   color : white;
   position : relative;
+  margin: 10vh 0% 0% 0%;
+
 `;
+
+export const SectionBlock3 = styled.div`
+  width: 90%;
+  height: auto;
+  display: flex;
+  flex-flow : row wrap;
+  justify-content: center;
+  align-items: center;
+  color : white;
+  position : relative;
+
+  margin: 15% 0% 0% 0%;
+  z-index : 1;
+  `;
+
 export const GrenBlock = styled.div`
-  width: 100%;
-  height: 30vh;
+  width: 90%;
+  height: auto;
   display: flex;
   flex-flow : row wrap;
   justify-content: center;
@@ -49,19 +69,30 @@ export const HannaTxt = styled.p`
   font-family : "한나";
   font-size : 5vmin;
   width: 100%;
+  line-height : 200%;
+  font-weight : 600;
 `;
 export const StrongTxt = styled.p`
-  font-family : "워헤븐";
+  font-family : "한나프로";
   font-size : 7vmin;
   width: 100%;
   color : #40FF00;
+  line-height : 250%;
+  `;
+
+  export const HannaProTxt = styled.p`
+  font-family : "한나프로";
+  font-size : 5vmin;
+  width: 100%;
+  line-height : 200%;
   `;
 export const BackGroundGroup = styled.div`
   height: auto;
   background-image: linear-gradient(
+    rgba(0, 0, 0, 0.95), 
     rgba(0, 0, 0, 0.9), 
-    rgba(0, 0, 0, 0.6), 
-    rgba(0, 0, 0, 0.6), 
+    rgba(0, 0, 0, 0.7), 
+    rgba(0, 0, 0, 0.7), 
     rgba(0, 0, 0, 0.95)
   ),
     url(${({ BackGroundImage }) => BackGroundImage});
@@ -90,11 +121,64 @@ align-items: center;
 text-align:center;
 background-color: rgb(50,50,50,0.5);
 overflow-y: auto;
-padding: 3%;
+padding: 8%;
 overflow : hidden;
 border-radius : 20px;
-margin: 2% 0% 0% 0%;
 box-shadow: inset 0px 0px 15px 15px rgb(204,204,204,0.2);
+z-index : 1;
+position : relative;
+margin: 5% 0%;
+`;
+
+export const TextGruop2 = styled.div`
+display: flex;
+width : 80%;
+height :auto;
+flex-flow : row wrap;
+justify-content: flex-start;
+align-items: center;  
+text-align:center;
+background-color: rgb(0,0,0,0.3);
+overflow-y: auto;
+padding: 10%;
+overflow : hidden;
+border-radius : 20px;
+box-shadow: inset 0px 0px 10px 10px rgb(204,0,0,0.4);
+z-index : 1;
+position : relative;
+margin: 5% 0%;
+`;
+
+export const TextGruop3 = styled.div`
+display: flex;
+width : 100%;
+height :auto;
+flex-flow : row wrap;
+justify-content: flex-start;
+align-items: center;  
+text-align:center;
+background-color: rgb(204,0,0);
+overflow-y: auto;
+padding: 0% 8%;
+overflow : hidden;
+border-radius : 20px;
+margin: 5% 0%;
+z-index : 1;
+position : relative;
+`;
+
+export const TransText = styled.div`
+display: flex;
+width : 100%;
+flex-flow : row wrap;
+justify-content: flex-start;
+align-items: center;  
+text-align:center;
+background-color: transparent;
+overflow-y: auto;
+padding: 8% 0%;
+overflow : hidden;
+margin: 5% 0%;
 `;
 export const RedSpan = styled.span`
 color : #FF0000;
@@ -110,11 +194,7 @@ color : #FFFF00;
 
 `
 
-export const GreenSpan = styled.span`
-color : #58FAF4;
-font-weight : 600;
 
-`
 export const BlueSpan = styled.span`
 color : #DA81F5;
 font-weight : 600;
@@ -125,7 +205,88 @@ color : #8181F7;
 font-weight : 600;
 `
 
-const GraySpan = styled.span`
+export const GraySpan = styled.span`
 
 color : #A4A4A4;
 `
+
+export const GradientButton = styled.button`
+  
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  font-size: 5vmin;
+  font-family : '스윗';
+  border-radius: 25px;
+  cursor: pointer;
+  transition: background 0.3s ease;
+  margin : 5% 0% 15% 0%;
+  box-shadow: 0px 4px 0px 0px #ff7300;
+  background: linear-gradient(45deg, #ff7300, #feac5e);
+  width : 90%;
+`;
+
+export const Balloon = styled.div`
+  position: relative;
+	background: linear-gradient(45deg, #006600, #33CC33);
+	border-radius: .4em;
+  padding : 5%;
+  box-shadow: 1px 3px 0px 0px #339933;
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 50%;
+    width: 0;
+    height: 0;
+    border: 20px solid transparent;
+    border-right-color: #006600;
+    border-left: 0;
+    border-bottom: 0;
+    margin-top: -10px;
+    margin-left: -20px;
+  }
+
+  animation: FloatAnimation 2s ease infinite; /* 애니메이션을 적용합니다. */
+
+  @keyframes FloatAnimation{
+    0% {
+      transform: translateY(0);
+    }
+    50% {
+      transform:translateY(-10px);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
+`;
+
+
+export const ImageContainer = styled.div`
+
+width: ${({ width }) => width};
+height: auto;
+opacity : 60%;
+object-fit: cover;
+background-color: transparent;
+transform : rotate(${({ rotate }) => rotate});
+top:${({ top }) => top};
+left: ${({ left }) => left};
+position: absolute; /* 겹치는 요소에 position 추가 */
+z-index : 0;
+`;
+
+export const ImgComponent = ({ imagePath, top, left,rotate,width }) => {
+  return (
+      <ImageContainer 
+        top = {top}
+        left = {left}
+        rotate={rotate}
+        width={width}
+      >
+          <img src={`${process.env.PUBLIC_URL }/imgData/${imagePath}`} alt="페이지 이미지" style={{ maxWidth: '100%', maxHeight: '100%' }}/>
+      </ImageContainer>
+      
+  );
+};
